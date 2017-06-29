bf30936048a92380cb5d6252815d0e6e138261c9
Kiran_totad <kiran.totad@nectechnologies.in>
2017-06-29 12:56:47 +0530

    Remove log translations
    Log messages are no longer being translated. This removes all use of the
    _LE, _LI, and _LW translation markers to simplify logging and to avoid
    confusion with new contributions.
    
    See:
    http://lists.openstack.org/pipermail/openstack-i18n/2016-November/002574.html
    http://lists.openstack.org/pipermail/openstack-dev/2017-March/113365.html
    
    Change-Id: Ic22cf715a70e9e1ff734944015d2f8a1ab2dbef8

--- cloudkittyclient/i18n.py
+++ cloudkittyclient/i18n.py
@@ -22,7 +22,3 @@ _translators = i18n.TranslatorFactory(domain='cloudkittyclient')
 i18n.enable_lazy()
 
 _ = _translators.primary
-_LI = _translators.log_info
-_LW = _translators.log_warning
-_LE = _translators.log_error
-_LC = _translators.log_critical
