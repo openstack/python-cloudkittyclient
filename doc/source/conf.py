296fd22640eb9c27814e5c4a9ef505fd8730f903
manchandavishal <manchandavishal143@gmail.com>
2019-09-16 16:08:04 +0000

    Generate PDF documentation
    This commit adds a new tox target to build PDF documentation.
    It's a part of community goal, see storyboard for more
    information.
    
    Change-Id: I6e4f6372ff026a9eb379ee06c683451f58a99976
    Story: 2006075
    Task: 34812

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,6 +23,7 @@ extensions = [
     'cliff.sphinxext',
     'sphinx.ext.autodoc',
     'openstackdocstheme',
+    'sphinxcontrib.rsvgconverter',
 ]
 
 autoprogram_cliff_ignored = [
@@ -74,14 +75,30 @@ html_last_updated_fmt = '%Y-%m-%d %H:%M'
 # Output file base name for HTML help builder.
 htmlhelp_basename = '%sdoc' % project
 
+
+# -- Options for LaTeX output ---------------------------------------------
+
+# Disable usage of xindy https://bugzilla.redhat.com/show_bug.cgi?id=1643664
+latex_use_xindy = False
+
+latex_domain_indices = False
+
+latex_elements = {
+    'makeindex': '',
+    'printindex': '',
+    'preamble': r'\setcounter{tocdepth}{3}',
+}
+
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass
 # [howto/manual]).
+# NOTE: Specify toctree_only=True for a better document structure of
+# the generated PDF file.
 latex_documents = [
     ('index',
-     '%s.tex' % project,
+     'doc-%s.tex' % project,
      u'%s Documentation' % project,
-     u'OpenStack Foundation', 'manual'),
+     u'OpenStack Foundation', 'howto', True),
 ]
 
 # Example configuration for intersphinx: refer to the Python standard library.
