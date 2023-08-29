244f229af7cb95b82118da9a4873806942a895ef
Rafael Weingärtner <rafael@apache.org>
2023-08-29 09:44:38 -0300

    Fix passenv declaration in tox.ini and function tests python env
    While running the tests with the latest tox I was getting the following error message:
    ```
    failed with pass_env values cannot contain whitespace, use comma to have multiple values in a single line'
    
    ```
    
    That error is happening because of the passenv declaration. This patch is proposing a fix for that.
    
    Besides the `tox` issue, we also needed to create a patch for the use of virtual env inside DevStack.
    This patches presents a solution to run tests using the virtual env of DevStack.
    
    Change-Id: Id8249ebb15d4047dcc6181908eae66eb39722863

--- cloudkittyclient/tests/functional/base.py
+++ cloudkittyclient/tests/functional/base.py
@@ -15,26 +15,57 @@
 #
 import json
 import os
-import shlex
 import subprocess
 
 from cloudkittyclient.tests import utils
 
+from oslo_log import log
+
+LOG = log.getLogger(__name__)
+
 
 class BaseFunctionalTest(utils.BaseTestCase):
+
+    # DevStack is using VENV by default. Therefore, to execute the commands,
+    # we need to activate the VENV. And, to do that, we need the VENV path.
+    # This path is hardcoded here because we could not find a variable in this
+    # Python code to retrieve the VENV variable from the test machine.
+    # It seems that because of the stack TOX -> stestr -> this python code, and
+    # so on, we are not able to access the DevStack variables here.
+    #
+    # If somebody finds a solution, we can remove the hardcoded path here.
+    DEV_STACK_VENV_BASE_PATH = "/opt/stack/data/venv"
+
+    BASE_COMMAND_WITH_VENV = "source %s/bin/activate && %s "
 
     def _run(self, executable, action,
              flags='', params='', fmt='-f json', stdin=None, has_output=True):
         if not has_output:
             fmt = ''
+
+        does_venv_exist = not os.system("ls -lah /opt/stack/data/venv")
+        LOG.info("Test to check if the VENV file exist returned: [%s].",
+                 does_venv_exist)
+
+        system_variables = os.environ.copy()
+        LOG.info("System variables [%s] found when executing the tests.",
+                 system_variables)
+
         cmd = ' '.join([executable, flags, action, params, fmt])
-        cmd = shlex.split(cmd)
+
+        actual_command_with_venv = self.BASE_COMMAND_WITH_VENV % (
+            self.DEV_STACK_VENV_BASE_PATH, cmd)
+
+        LOG.info("Command being executed: [%s].", actual_command_with_venv)
+
         p = subprocess.Popen(
-            cmd, env=os.environ.copy(), shell=False,
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-            stdin=subprocess.PIPE if stdin else None,
+            ["bash", "-c", actual_command_with_venv],
+            env=os.environ.copy(), shell=False, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE, stdin=subprocess.PIPE if stdin else None
         )
         stdout, stderr = p.communicate(input=stdin)
+        LOG.info("Standard output [%s] and error output [%s] for command "
+                 "[%s]. ", stdout, stderr, actual_command_with_venv)
         if p.returncode != 0:
             raise RuntimeError('"{cmd}" returned {val}: {msg}'.format(
                 cmd=' '.join(cmd), val=p.returncode, msg=stderr))
